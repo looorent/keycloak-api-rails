a2916c79c13453805f82b7c4f0f8fd920c62820f
Lorent Lempereur <lorent@commuty.net>
2018-01-15 13:27:50 +0100

    Version 0.2

--- lib/keycloak-api-rails/service.rb
+++ lib/keycloak-api-rails/service.rb
@@ -24,7 +24,7 @@ module Keycloak
       end
     rescue JSON::JWT::VerificationFailed => e
       raise TokenError.verification_failed(token, e)
-    rescue JWK::Set::KidNotFound => e
+    rescue JSON::JWK::Set::KidNotFound => e
       raise TokenError.verification_failed(token, e)
     rescue JSON::JWT::InvalidFormat
       raise TokenError.invalid_format(token, e)
